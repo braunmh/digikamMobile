a02dccc6a1d422bf42d1f43cd97994bc8f7c5da1
mbraun <braun.h.michael@gmail.com>
2024-01-06 11:52:13 +0100

    Imports bereinigt

--- lib/dialog/settings_dialog.dart
+++ lib/dialog/settings_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../drawer_widget.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
